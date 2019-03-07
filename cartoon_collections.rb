b24d0b76c7dd80f54faa1764ce33f2d59befce9e
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-03-07 07:51:21 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,6 +15,7 @@ end
 def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find("cheddar", "gouda", "camembert")
   if array == cheese_types
     puts
 else
